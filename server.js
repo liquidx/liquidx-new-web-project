37de12a432561c5b190d89290eb11b782ef1860c
Alastair Tse <alastair@liquidx.net>
2019-12-08 19:08:35 +0900

    Default to a const port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var express = require('express');
+const express = require('express')
 
-var app = express();
-app.use(express.static('public'));
+const app = express()
+app.use(express.static('public'))
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('liquidxnet listening on port ' + listener.address().port);
+let port = process.env.PORT || 5001
+
+var listener = app.listen(port, function () {
+  console.log('listening on port ' + listener.address().port);
   console.log('http://localhost:' + listener.address().port);
 });
