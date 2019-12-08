dd4ed29c0bead881e8a72736de8db31614c07cb2
Alastair Tse <alastair@liquidx.net>
2019-12-08 18:04:50 +0900

    Initial commit. Firebase and Gcloud support.

---
+++ README.md
@@ -0,0 +1,2 @@
+Attempt at creating a basic node or static-site template that could be deployed to Firebase, Google App Engine or Zeit.
+
---
+++ server.js
@@ -0,0 +1,10 @@
+// Only used for serving off glitch.
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('liquidxnet listening on port ' + listener.address().port);
+  console.log('http://localhost:' + listener.address().port);
+});
